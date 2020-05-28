4b7ce856fb203b7bb66b6dc3c4bcdad01ab729a4
XSSS12680 <65147908+XSSS12680@users.noreply.github.com>
2020-05-28 21:33:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,11 +11,26 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.111/g' package/base-files/files/bin/config_generate
 
-#删除默认密码
-#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+#cd openwrt
+rm -Rf package/lean/luci-theme-argon
 
-#更改时区
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+#自定插件
+#主题
+
+git clone https://github.com/jerrykuku/luci-theme-argon -b 18.06 package/luci-theme-argon
+git clone https://github.com/Leo-Jo-My/luci-theme-argon-light-mod package/luci-theme-argon-light-mod
+git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark  package/luci-theme-Butterfly-dark
+git clone https://github.com/Leo-Jo-My/luci-theme-argon-dark-mod package/luci-theme-argon-dark-mod
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone https://github.com/rosywrt/luci-theme-rosy.git ./package/luci-theme-rosy
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+
 
 #更新feeds
 ./scripts/feeds update -a
